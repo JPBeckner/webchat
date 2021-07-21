dc82b25c8714a683fd7bbc873607d632bd03d1fe
JPBeckner <pedrobeckner@hotmail.com>
2021-07-21 18:10:59 -0300

    feat(url): add bot routes

--- setup/urls.py
+++ setup/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 urlpatterns = [
     path('', include('chat.urls', namespace='chat')),
     path('user/', include('user.urls', namespace='user')),
+    path('bot/', include('bot.urls', namespace='bot')),
     path('admin/', admin.site.urls),
 ]
